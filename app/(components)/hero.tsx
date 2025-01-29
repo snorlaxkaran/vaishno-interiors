17350c15d078886022c5a077c64a88ead600d4b6
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-01-29 18:56:02 +0530

    po

--- app/(components)/hero.tsx
+++ app/(components)/hero.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Hero = () => {
@@ -12,8 +13,14 @@ const Hero = () => {
       }}
       className="relative"
     >
-      <div className="bg-white p-10 max-w-xl mx-24 top-1/2 absolute hidden md:block ">
-        <h1>Stunning interior design services, now within reach.</h1>
+      <div className="bg-[#f3f0eb] text-[#362b24] py-5 px-10 w-full lg:max-w-[46rem] eb-garamond tracking-wide absolute lg:top-1/2 lg:-translate-y-1/2 lg:ml-20 flex flex-col gap-10">
+        <p className="text-center uppercase font-[family-name:var(--font-geist-sans)] text-sm font-semibold">
+          Interior Design Service
+        </p>
+        <h1 className="lg:text-7xl text-5xl">
+          Stunning interior design services, now within reach.
+        </h1>
+        <Button className="start-0">hello</Button>
       </div>
     </div>
   );
