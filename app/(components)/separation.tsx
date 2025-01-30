94a70a5cf12800865eb7a85ee6d62de846d0bd8b
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-01-30 11:20:04 +0530

    svg

--- app/(components)/separation.tsx
+++ app/(components)/separation.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
 const Separation = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="bg-[#E0DFAF] text-[#362b24] ">
+      <div className=" max-w-6xl mx-auto m-4 grid grid-cols-1 eb-garamond md:grid-cols-2 ">
+        <h1 className="md:text-5xl text-2xl my-16">
+          Our interior design services are tailored to your space, style, and
+          budget.
+        </h1>
+        <Image
+          alt="svg"
+          width={100}
+          height={100}
+          unoptimized
+          src={"/package-bg.png"}
+          className="w-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Separation;
